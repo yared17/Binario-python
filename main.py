1f97f8ffb5d081b478d304b871d1dc434a0d1602
Yared Henriquez Blanquicett <30659662+yared17@users.noreply.github.com>
2021-11-12 12:50:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+Binario=int(input('Digite un numero para su conversion: '))
+print(''' Escoga una base de conversion:
+[1] convertir a Decimal
+[2] convertir a Hexagecimal
+[3] convertir a octal
+''')
+
+opcion = int(input('Opcion: '))
+if opcion ==1:
+   print('El numero {} en el convertidor para decimal es igual a {}'.format(Binario,(int(str(Binario),2))))
+elif opcion ==2:
+  print('El numero {} en el convertidor para octal es igual a {}'.format(Binario, oct(Binario)))
+elif opcion ==3:
+  print('El numero {} en el convertidor para Hexagecimal es igual a {}'.format(Binario, hex(Binario)))
+else: 
+  print('opcion invalida, intente nuevamente.')
+Numero=(input('Digite un numero para su conversion: '))
+print(''' Convertir a binario:
+[1] convertir de Decimal a binario
+[2] convertir de Octal a binario
+[3] convertir de Hexagecimal a binario''')
+
+conversion =int(input('Opcion:'))
+if conversion ==1:
+   print('El numero {} en el convertidor de decimal a binario es igual a {}'.format(Numero,(int(bin(int(Numero))[2:]))))
+elif conversion ==2:
+  print('El numero {} en el convertidor de octal a binario es igual a {}'.format(Numero,(int(bin(int(Numero, 8))[2:]))))
+elif conversion ==3:
+  print('El numero {} en el convertidor de Hexagecimal a Binario es igual a {}'.format(Numero,(int(bin(int(Numero , 16))[2:]))))
+else: 
+  print('opcion invalida, intente nuevamente.')
+
+  
+  
